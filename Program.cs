7e7fbfccaa6019fb288185f993bd1a25d9802be9
60steose@gafe.molndal.se <60steose@gafe.molndal.se>
2026-02-19 10:30:28 +0100

    Implement Service Layer

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
+using CloudSoft.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<INewsletterService, NewsletterService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
